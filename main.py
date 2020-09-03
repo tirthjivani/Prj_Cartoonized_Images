6cc92dd5e5a19d3f7eef60cacd49c9d318b96907
Tirth Jivani <tirthjivani17@gmail.com>
2020-09-03 12:11:47 +0530

    main.py addedf

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2 
+import numpy as np 
+import sys
+
+if len(sys.argv) != 2:
+	print("Usage: python main.py pic.jpg")
+	exit(0)
+
+try:
+	img = cv2.imread(sys.argv[1]) 
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+	gray = cv2.medianBlur(gray, 5) 
+	edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, 
+											cv2.THRESH_BINARY, 9, 9) 
+
+	color = cv2.bilateralFilter(img, 9, 250, 250) 
+	cartoon = cv2.bitwise_and(color, color, mask=edges) 
+
+	cv2.namedWindow("Cartoon", cv2.WINDOW_NORMAL)
+	cv2.namedWindow("Edges", cv2.WINDOW_NORMAL)
+	cv2.imshow("Edges", edges) 
+	cv2.imshow("Cartoon", cartoon) 
+	cv2.waitKey(0) 
+	cv2.destroyAllWindows() 
+
+except cv2.error:
+	print("Error: Either no such image found or this file doesn't support the code.")
